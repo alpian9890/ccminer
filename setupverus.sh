ab91446ac8e0164456350056790c9aa2e97da0eb
alpian9890 <43318972+alpian9890@users.noreply.github.com>
2024-11-22 11:16:45 +0700

    ccminer-termux

---
+++ setupverus.sh
@@ -0,0 +1,21 @@
+clear
+echo "Settingup..."
+yes | pkg update && pkg upgrade && apt update && apt upgrade
+yes | pkg install libjansson wget nano
+mkdir $HOME/ccminer && cd $HOME/ccminer
+#wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer
+#wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/config.json
+#wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/start.sh
+
+
+
+chmod +x $HOME/ccminer $HOME/ccminer/*
+#nano $HOME/ccminer/config.json
+
+#Lanjut setting wallet dll
+#Cara setting autorun :
+# cd && cd && cd && nano ../usr/etc/bash.bashrc
+#Copykan ini kebaris paling bawah,lalu ctrl X, simpan pilih Y enter
+echo "cd $HOME/ccminer/&&./start.sh"  >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo "\n \n"
+cat /data/data/com.termux/files/usr/etc/bash.bashrc
